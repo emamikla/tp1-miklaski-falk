8e712796117893a52da8c1fccc8095ea262c4d94
emamikla <ema.miklaski@gmail.com>
2026-04-17 20:00:33 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp1_miklaski_falk.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
