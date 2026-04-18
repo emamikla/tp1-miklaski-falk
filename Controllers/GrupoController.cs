1817c2fb34c5e5d20292d4a0679df42375012194
emamikla <ema.miklaski@gmail.com>
2026-04-18 13:51:55 -0300

    intento de avance

--- Controllers/GrupoController.cs
+++ Controllers/GrupoController.cs
@@ -5,3 +5,16 @@ using tp1_miklaski_falk.Models;
 namespace tp1_miklaski_falk.Controllers;
 
 public class GrupoController : Controller
+{
+    public IActionResult Index()
+    {
+        Grupo grupo = new Grupo();
+        ViewBag.grupo = grupo;
+        return View();
+    }
+
+    public IActionResult selectIntegrante(int dni)
+    {
+        
+    }
+}
